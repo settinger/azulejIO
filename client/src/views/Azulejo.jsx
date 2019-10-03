2e1a0dcda76d60cbc657efd73c0be89a9823d1f6
Sam Ettinger <ettingersam@gmail.com>
2019-10-03 11:43:16 +0100

    Left a note for what to do next

--- client/src/views/Azulejo.jsx
+++ client/src/views/Azulejo.jsx
@@ -5,13 +5,25 @@ export default class Azulejo extends Component {
   constructor(props) {
     super(props);
     this.state = {};
-    this.state.canvas = null;
+    this.saveToAccount = this.saveToAccount.bind(this);
   }
+
+  saveToAccount() {
+    const img = this.$canvas.toDataURL("image/png");
+    // To do: pass this img to the backend, where it then gets uploaded to cloudinary
+  }
+
+  componentDidMount() {
+    this.$canvas = document.getElementById("drawing-canvas");
+  }
+
   render() {
     return (
       <div>
         <AzulejoCanvas />
-        {this.props.user && <button>Save to my account</button>}
+        {this.props.user && (
+          <button onClick={this.saveToAccount}>Save to my account</button>
+        )}
       </div>
     );
   }
