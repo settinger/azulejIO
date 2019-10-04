99c202b20413dc21acbebfb5f20b528563b9ca36
Sam Ettinger <ettingersam@gmail.com>
2019-10-04 11:18:29 +0100

    Added links to thumbnail

--- client/src/components/AzulejoThumbnail.jsx
+++ client/src/components/AzulejoThumbnail.jsx
@@ -15,29 +15,23 @@ const AzulejoThumbnail = props => {
         <img src={thumbUrl} alt="Azulejo thumbnail" className="card-img-top" />
       </Link>
       <div className="card-body">
-        <h5 className="card-title">{props.name}</h5>
-        <p className="card-text">By {props.createdBy}</p>
+        <h5 className="card-title">
+          <Link to={`/azulejo/${props.id}`}>{props.name}</Link>
+        </h5>
         <p className="card-text">
-          {props.colors.includes("Red") && (
-            <span style={{ color: "red" }}>■</span>
-          )}
-          {props.colors.includes("Orange") && (
-            <span style={{ color: "orange" }}>■</span>
-          )}
-          {props.colors.includes("Yellow") && (
-            <span style={{ color: "yellow" }}>■</span>
-          )}
-          {props.colors.includes("Green") && (
-            <span style={{ color: "green" }}>■</span>
-          )}
-          {props.colors.includes("Blue") && (
-            <span style={{ color: "blue" }}>■</span>
-          )}
-          {props.colors.includes("Purple") && (
-            <span style={{ color: "purple" }}>■</span>
-          )}
-          {props.colors.includes("Black") && (
-            <span style={{ color: "black" }}>■</span>
+          By <Link to={`/profile/${props.createdBy}`}>{props.createdBy}</Link>
+        </p>
+        <p className="card-text">
+          {["Red", "Orange", "Yellow", "Green", "Blue", "Purple", "Black"].map(
+            color => {
+              return (
+                props.colors.includes(color) && (
+                  <span key={color} style={{ color: color.toLowerCase() }}>
+                    ■
+                  </span>
+                )
+              );
+            }
           )}
         </p>
       </div>
